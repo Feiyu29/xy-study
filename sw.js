389b22eb17c70ad0da6379c5eac414551a0254a8
Feiyu29 <feiyu29@users.noreply.github.com>
2026-07-30 18:55:12 +0800

    赫赫小学学习工作台

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='xy-study-v1';
+const ASSETS=['./','./index.html','./poems-data.js','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{});return r}).catch(()=>caches.match(e.request)))});
